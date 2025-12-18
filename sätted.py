79c15e06145f68c8bc671fa1cf5201c0c05f8107
tm-Haas <haastuulimia@gmail.com>
2025-12-18 03:52:20 +0200

    Update sätted.py

--- sätted.py
+++ sätted.py
@@ -1,19 +1,51 @@
 #Siin elavad konstandid
-AKEN_LAIUS = 600
-AKEN_KÕRGUS = 500
+AKNA_NIMI = "AvoiDER - Stardi menüü"
+AKEN_LAIUS = 736
+AKEN_KÕRGUS = 414
 FPS = 60
-VÄRSKENDUSVÄRV = "yellow"
-ASTEROIDI_KIIRUS = 45
-VAENLASE_KIIRUS = 10
+MENÜÜ_TAUST = "pildid/alg_taust.jpg"
+TAUST = "pildid/taust.jpg"
+ASTEROIDI_KIIRUS = 0.5
+VAENLASE_KIIRUS = 0.4
+MÄNGIJA_KIIRUS = 5
 ASTEROIDI_SUURUSED = ["Suur", "Keskmine", "Väike"]
 AKNA_NIMI = "Proov"
-ALUMINE_ÄÄR = AKEN_LAIUS + 10
+ALUMINE_ÄÄR = AKEN_KÕRGUS + 100
 ÜLEMINE_ÄÄR = AKEN_KÕRGUS - 10
-MIN_ASTEROIDIDE_ARV = 3
-MAX_ASTEROIDIDE_ARV = 15
-MIN_VAENLASTE_ARV = 5
-MAX_VAENLASTE_ARV = 12
+MIN_ASTEROIDIDE_ARV = 2
+MAX_ASTEROIDIDE_ARV = 5
+MIN_VAENLASTE_ARV = 2
+MAX_VAENLASTE_ARV = 5
 KOKKUPÕRGE_RECT_LIIGE= 2
 VAENLASE_VÄÄRTUS = 1
-FONT = "comici.ttf"
-FONT_VÄRV = "black"
+FONT = "PIXgothic_7shareware.ttf"
+
+NUPU_BG_VÄRV = (185, 208, 230, 128)
+NUPU_BG_HOVER = (225, 208, 230, 180)
+MENÜÜ_PEALKIRI = "#b68f40"
+NUPUD = "#d7fcd4"
+NUPUD_HOVER = "White"
+
+OLEK_MÄNGI = "AvoiDER - Mängi"
+OLEK_EDETABEL = "AvoiDER - Edetabel"
+OLEK_MÄNG = "AvoiDER - Tegelase valik"
+
+TEGELASED = {
+    "Kosmoselaev": {
+        "pilt": "pildid/kosmoselaev.png",
+        "kuul": "pildid/tava_kuul.png",
+        "kiirus_korrutaja": 1.0
+    },
+
+    "Tähtede_Miku": {
+        "pilt": "pildid/Tähtede_Miku.png",
+        "kuul": "pildid/tähe_kuul.png",
+        "kiirus_korrutaja": 1.2
+    },
+
+    "Kosmosekass": {
+        "pilt": "pildid/Kosmosekass.png",
+        "kuul": "pildid/kala_kuul.png",
+        "kiirus_korrutaja": 0.9
+    }
+}
